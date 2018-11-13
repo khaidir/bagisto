fa1186143e8812b5f48b87b97962529ef487c393
rahul shukla <rahulshukla.symfony517@webkul.com>
2018-11-13 15:23:27 +0530

    customer & review changes

--- packages/Webkul/Admin/src/Exceptions/Handler.php
+++ packages/Webkul/Admin/src/Exceptions/Handler.php
@@ -66,14 +66,13 @@ class Handler extends ExceptionHandler
                 return response()->view('shop::errors.500', [], 500);
             }
         }
-        // else if ($exception instanceof ErrorException) {
+        // else if ($exception instanceof \ErrorException) {
 
         //     if(strpos($_SERVER['REQUEST_URI'], 'admin') !== false){
         //         return response()->view('admin::errors.500', [], 500);
         //     }else {
         //         return response()->view('shop::errors.500', [], 500);
         //     }
-
         // }
 
         return parent::render($request, $exception);
